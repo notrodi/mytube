3d99531a778fa0dda2cd295821ac5df1608c795b
rodisite <66669407+rodisite@users.noreply.github.com>
2022-04-26 13:57:16 +0300

    Созданы компоненты видео и коментариев, а также сервис видео

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VideosComponent } from './videos/videos.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideosComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule
